9b5039584ead2f8280fe924576c794e62fb8729c
Reuben Frankel <rfrankel@matatika.com>
2025-03-17 10:48:30 +0000

    Ignore no records for sprint results stream, since no sprint races have happened yet

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,9 +2,9 @@
 
 from datetime import date
 
-from singer_sdk.testing import get_tap_test_class
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
-from tap_f1.tap import TapF1
+from tap_f1.tap import TapF1, streams
 
 SAMPLE_CONFIG = {
     "start_date": date(2025, 1, 1).isoformat(),
@@ -15,4 +15,7 @@ SAMPLE_CONFIG = {
 TestTapF1 = get_tap_test_class(
     tap_class=TapF1,
     config=SAMPLE_CONFIG,
+    suite_config=SuiteConfig(
+        ignore_no_records_for_streams=[streams.SprintResultsStream.name]
+    ),
 )
