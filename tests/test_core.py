5d66fb1c8036ada758ff9c27e6b9745637a943ba
Reuben Frankel <rfrankel@matatika.com>
2025-03-17 10:33:22 +0000

    Test with 2025 season data

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,7 +7,7 @@ from singer_sdk.testing import get_tap_test_class
 from tap_f1.tap import TapF1
 
 SAMPLE_CONFIG = {
-    "start_date": date(2024, 1, 1).isoformat(),
+    "start_date": date(2025, 1, 1).isoformat(),
 }
 
 
